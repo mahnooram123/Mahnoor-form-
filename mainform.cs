ec252c18db73201c2f0da3c8e0c636d9429403a2
mahnooram123 <mahnoormustafa640@gmail.com>
2025-11-02 15:52:38 +0500

    Add files via upload

---
+++ mainform.cs
@@ -0,0 +1,20 @@
+﻿using Booksystem;
+using System;
+using System.Windows.Forms;
+
+namespace Booksystem
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            LibraryData.InitializeSampleBooks();
+
+            Application.Run(new LoginForm());
+        }
+    }
+}
