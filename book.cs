dfc5b4e8e2e83c4e0e7a62001cd7901568648f29
mahnooram123 <mahnoormustafa640@gmail.com>
2025-11-02 15:41:24 +0500

    Add files via upload

---
+++ book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Booksytsem
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public bool IsAvailable { get; set; } = true;
+
+
+        public string Availability => IsAvailable ? "Available" : "Borrowed";
+    }
+}
