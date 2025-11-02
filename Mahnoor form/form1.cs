3491d549e46e9593bfe30b02d4912f39eb73c919
mahnooram123 <mahnoormustafa640@gmail.com>
2025-11-02 15:59:14 +0500

    Add files via upload

---
+++ Mahnoor form/form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click_1(object sender, EventArgs e)
+        {
+            {
+                if (txtUsername.Text == "admin" && txtPassword.Text == "123")
+                {
+                    this.Hide();
+                    Form2 calcForm = new Form2();
+
+                    calcForm.FormClosed += (s, args) => this.Close();
+                    calcForm.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid username or password!");
+                }
+            }
+
+
+        }
+    }
+}
